cf45b8ac360edf208f18e669d8e5b5afc388e272
yuansan <11@11>
2022-05-21 11:19:14 +0800

    11111

--- Config.cs
+++ Config.cs
@@ -95,7 +95,7 @@ namespace Hotfix.Common
 			//hosts.Add("192.168.101.221", 16000);
 			//国内测试服
 			//hosts.Add("150.158.55.161", 16000);
-			hosts.Add("127.0.0.1", 8990);
+			hosts.Add("192.168.101.106", 8990);
 			//hosts.Add("47.100.172.186", 8990);
 			{
 				GameConfig game = new GameConfig();
